de26387e434be908f25ff81a268375576ffa4a85
Dylan Hays <twentygx@outlook.com>
2023-08-08 17:39:58 -0500

    Initial homepage

--- p4-client/src/App.js
+++ p4-client/src/App.js
@@ -1,23 +1,67 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
+import ViewFacilityInfo from './Components/FacilityViews/ViewFacilityInfo';
+import Sidebar from './Components/Sidebar';
+import UserRequestList from './Components/UserRequests/UserRequestList';
+import MainContent from './Components/MainContent';
+import { UserContext, UserProvider } from './Context/UserContext';
+import './Common.css'
+import ExpandingButton from './Components/ExpandingButton';
+
+
 
 function App() {
+  const history = useHistory();
+
+  function onLogout() {
+    history.push('/')
+  }
+
+  const SiteAdmin = [
+    { Name: "User Requests", Location: "/admin/user_requests" }
+  ]
+
+  const CorporateInfo = [
+    { Name: "Corporate Schedule", Location: "/home" },
+    { Name: "Corporate Time Records", Location: "/home" },
+    { Name: "Field Staff Directory", Location: "/home" },
+    { Name: "Corporate Staff Directory", Location: "/home" },
+    { Name: "Facility Directory", Location: "/home" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProvider>
+        <div className='primary-layout'>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route>
+              <div className='new-header'>
+                <div className='standard-flex header-content'>
+                  <img src="http://10.1.1.37/favicon.ico" alt='LTCData Logo' />
+                  <h1 className='centered-text'>LTCData Processing</h1>
+                  <nav className='general-flex right-align'>
+                    <ExpandingButton text="Site Admin" children={SiteAdmin} />
+                    <ExpandingButton text="Corporate" children={CorporateInfo} />
+                    <div onClick={onLogout}>
+                      <ExpandingButton text="Logout" primary_location={'/'} />
+                    </div>
+                  </nav>
+                </div>
+              </div>
+              <div className='main-content'>
+                <MainContent />
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </UserProvider>
     </div>
   );
 }
