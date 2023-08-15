59754d3f7115c27c49ae62ac6c9efc5b5f82d04b
Dylan Hays <twentygx@outlook.com>
2023-08-15 16:28:47 -0500

    added #destroy

--- app/controllers/master_security/facility_accesses_controller.rb
+++ app/controllers/master_security/facility_accesses_controller.rb
@@ -1,5 +1,5 @@
 class MasterSecurity::FacilityAccessesController < ApplicationController
-  before_action :set_facility_access, only: %i[show update]
+  before_action :set_facility_access, only: %i[show update destroy]
   before_action { ApplicationController.authenticate(session) }
 
   # GET /facility_accesses
@@ -12,6 +12,10 @@ class MasterSecurity::FacilityAccessesController < ApplicationController
   # GET /facility_accesses/1
   def show
     render json: @facility_access
+  end
+
+  def destroy
+    @facility_access.Access_Until = DateTime.now - 1.day
   end
 
   private
