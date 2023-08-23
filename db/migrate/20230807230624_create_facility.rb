a98233e4c2972009e5bab42305d31b4fbbc886c3
TheYearIsTwentyGX <twentygx@outlook.com>
2023-08-23 12:06:31 -0500

    Changed Coserial to be primary key as intended

--- db/migrate/20230807230624_create_facility.rb
+++ db/migrate/20230807230624_create_facility.rb
@@ -1,7 +1,6 @@
 class CreateFacility < ActiveRecord::Migration[6.1]
   def change
-    create_table :facilities do |t|
-      t.integer :Coserial
+    create_table :facilities, primary_key: :Coserial do |t|
       t.string :Report_Name
       t.string :Discipline
 
