4aa087283fbffe99631e6ebc0f9d391513a1892f
TheYearIsTwentyGX <twentygx@outlook.com>
2023-09-18 14:29:13 -0500

    Error handling and disable CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
+require "awesome_print"
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
+  before_action :log_request
+  before_action :disable_cors
+
+  def disable_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
+    headers["Access-Control-Request-Method"] = "*"
+    headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  end
+
+  def log_request
+    puts "Request: #{request.method} #{request.path}"
+    if params.keys.length == 2
+      puts "Params: #{params}"
+    end
+  end
 
   wrap_parameters format: [:json]
   def self.authenticate(session)
@@ -8,6 +26,15 @@ class ApplicationController < ActionController::API
 
   def authorize
     render json: {error: "Unauthorized"}, status: :unauthorized unless session.include? :user_id
+  end
+
+  def look_at_me_im_a_target(record, error_type = :unprocessable_entity)
+    full_errors = record&.errors&.full_messages || []
+    logger.look_at_me_im_a_target.ap full_errors
+    case error_type
+    when :unprocessable_entity
+      ErrorMailer.unprocessable_entity_email(record, params, session[:user_id]).deliver_now
+    end
   end
 
   def self.local_time
